89afaca30848176d5ed568ae76c5dc94edb168c8
hwangso595 <57157860+hwangso595@users.noreply.github.com>
2020-06-28 12:26:58 -0400

    move private information into dev.js

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://hwangso595:asdf@cluster0-jsyn1.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const config = require('./config/key');
+
+const { User } = require('./models/user');
+
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true
 })
 const db = mongoose.connection;
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'));
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
 app.get('/', (req,res) => {
     res.send('hello world');
+})
+
+app.post('/api/users/register', (req,res) => {
+    const user = new User(req.body);
+    user.save((err, userData) => {
+        if(err) return res.json({success: false, err})
+    })
+    return res.status(200)
 })
 
 app.listen(5000);
